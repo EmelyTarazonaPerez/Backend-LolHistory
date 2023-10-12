8d019279043ff975b111327509f77abd527ab584
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-10-12 15:52:35 -0500

    first commit

---
+++ src/main/java/proyecto/lolHistory/LolHistoryApplication.java
@@ -0,0 +1,13 @@
+package proyecto.lolHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolHistoryApplication {
+	public static void main(String[] args) {
+
+
+	}
+
+}
