649b29649ce9aaa97ff4508b85347af6604b8980
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-10-13 19:12:59 -0500

    hexagonal architecture

---
+++ src/main/java/LolHistory/infraestructura/rest/controller/UserController.java
@@ -0,0 +1,20 @@
+package LolHistory.infraestructura.rest.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/accounts")
+public class UserController {
+
+    @GetMapping("/{gameName}/{tagLine}")
+    public String getInvocador (@PathVariable Map<String, String> pathVarsMap){
+        String gameName  = pathVarsMap.get("gameName");
+        String tagLine = pathVarsMap.get("tagLine");
+        return "aquí el " + gameName + " " + tagLine;
+    }
+
+}
---
+++ src/test/java/LolHistory/ApplicationTests.java
@@ -0,0 +1,13 @@
+package LolHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
