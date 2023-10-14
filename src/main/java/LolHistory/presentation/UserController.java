21f2b8c9405c6d92f636c74088c896b38f12d8f6
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-10-13 21:51:19 -0500

    implementando arquitectura en capas

--- src/main/java/LolHistory/presentation/UserController.java
+++ src/main/java/LolHistory/presentation/UserController.java
@@ -1,4 +1,4 @@
-package LolHistory.infraestructura.rest.controller;
+package LolHistory.presentation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
