e6535372c9c96f87fa0494cf6ac12b0740550e5f
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-10-24 22:09:02 -0500

    clear

--- src/main/java/LolHistory/bussine/externalServices/ConsumerGameHistory.java
+++ src/main/java/LolHistory/bussine/externalServices/ConsumerGameHistory.java
@@ -1,17 +1,13 @@
 package LolHistory.bussine.externalServices;
 
-import LolHistory.bussine.externalServices.model.InfoByParticipant;
 import LolHistory.bussine.externalServices.model.Match;
-import LolHistory.bussine.externalServices.model.Participant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 @Service
 public class ConsumerGameHistory extends ConsumerRiotService  {
@@ -42,27 +38,6 @@ public class ConsumerGameHistory extends ConsumerRiotService  {
         return loopResponse();
     }
 
-   /* public List<Participant> getListStatsByPlayer(){
-        List<Match> dataHistoryGame = getAllStats();
-
-        assert dataHistoryGame != null;
-
-        return dataHistoryGame.stream().map(this::statsPlayer).collect(Collectors.toList());
-    }
-
-    private Participant statsPlayer (Match n) {
-        List<Participant> participants = n.getInfo().getParticipants();
-        Participant participant = null;
-
-        for (Participant current : participants) {
-            if (current.getPuuid().equals(consumerUserService.getPUUID())) {
-                participant = current;
-                participant.setDate(timeStampToDate(n));
-                break;
-            }
-        }
-        return participant;
-    }*/
 
 
 }
